0bd0dfd13925ffe1af95a7529ae77cd5336257a6
Medido1 <medi1222@gmail.com>
2024-08-21 11:48:59 +0100

    Add function to calculate monthly repayments with mortgage type repayment

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const radioBtns = document.querySelectorAll(".radio_container");
+const myForm = document.querySelector(".my_form");
+const repayement = document.getElementById("repayment");
+const interestOnly = document.getElementById("interest_only");
+const interestRate = document.getElementById("interest");
+const mortgageTerm = document.getElementById("mortgage_term");
+const mortgageAmount = document.getElementById("mortgage_amount");
+const monthlyPaymentText = document.querySelector(".monthly_repayments");
 
 function toggleActive(element) {
   radioBtns.forEach((btn) => btn.classList.remove("clicked"));
   element.classList.add("clicked");
 }
 
+function calculateRepayments(e) {
+  e.preventDefault();
+  let interestRateValue = parseFloat(interestRate.value) / 100 / 12; /* Monthly Interest Rate  */
+  let mortgageTermValue = parseInt(mortgageTerm.value);
+  let mortgageAmountValue = parseFloat(mortgageAmount.value);
+  console.log(mortgageAmountValue)
+  let monthlyPayment = 0
+  if (repayement.checked) {
+    let r = interestRateValue 
+    let mortgageTermMonths = mortgageTermValue * 12; 
+    monthlyPayment = 
+    (mortgageAmountValue * r * (1 + r) ** mortgageTermMonths) / 
+      ((1 + r) ** mortgageTermMonths - 1);
+    monthlyPaymentText.textContent = `£${monthlyPayment.toFixed(2)}`;
+  }
+}
+
 radioBtns.forEach((btn) => {
   btn.addEventListener("click", () => toggleActive(btn))
 })
+
+myForm.addEventListener("submit", calculateRepayments);
