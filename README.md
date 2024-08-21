cb69441dd37b88a83a35c25e2fc39000efd52e47
Medido1 <medi1222@gmail.com>
2024-08-21 16:23:14 +0100

    Finished Project

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Frontend-Mentor-Mortgage-repayment-calculator
-This is a solution to Mortgage repayment calculator challenge on Frontend Mentor
+# Frontend Mentor - Mortgage repayment calculator solution
+
+This is a solution to the [Mortgage repayment calculator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/mortgage-repayment-calculator-Galx1LXK73).
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Your users should be able to:
+
+  - Input mortgage information and see monthly repayment and total repayment amounts after submitting the form
+  - See form validation messages if any field is incomplete
+  - Complete the form only using their keyboard
+  - View the optimal layout for the interface depending on their device's screen size
+  - See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+  ## Desktop 
+  ![](./images/screenshots/desktopss.png)
+
+  ## Desktop results
+
+  ![](./images/screenshots/resultss.png)
+
+  ## Mobile 
+  ![](./images/screenshots/mobiless.png)
+
+### Links
+
+- Solution URL: [solution](https://github.com/Medido1/Frontend-Mentor-Mortgage-repayment-calculator)
+- Live Site URL: [live site]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@Medido1](https://www.frontendmentor.io/profile/Medido1)
+- GitHub - [@Medido1](https://github.com/Medido1)
--- script.js
+++ script.js
@@ -10,6 +10,48 @@ const totalToPay = document.querySelector(".total");
 const clearBtn = document.querySelector(".clear_btn");
 const emptyResultContainer = document.querySelector(".empty_result_container");
 const resultContainer = document.querySelector(".result_container");
+const inputs = document.querySelectorAll(".input_grp input");
+const inputGrps = document.querySelectorAll(".input_grp");
+const errorMsgs = document.querySelectorAll(".error_msg");
+const radioBtnInputs = document.querySelectorAll("input[type='radio']")
+
+function showError(index){
+  const symbol = inputGrps[index].lastElementChild;
+  symbol.classList.add("error");
+  errorMsgs[index].classList.remove("hidden");
+}
+
+function removeError(index) {
+  const symbol = inputGrps[index].lastElementChild;
+  symbol.classList.remove("error");
+  errorMsgs[index].classList.add("hidden");
+}
+
+function showRadioError() {
+  const radioErrorMsg = document.querySelector("fieldset .error_msg");
+  radioErrorMsg.classList.remove("hidden");
+}
+
+function removeRadioError() {
+  const radioErrorMsg = document.querySelector("fieldset .error_msg");
+  radioErrorMsg.classList.add("hidden");
+}
+
+function checkForError() {
+  let errorFound = false;
+  inputs.forEach((input, index) => {
+    if (input.value.trim() === "") {
+      showError(index);
+      errorFound = true;
+    } 
+  })
+  let radioChecked = [...radioBtnInputs].some(input => input.checked)
+  if (!radioChecked) {
+    showRadioError();
+    errorFound = true;
+  }
+  return errorFound;
+}
 
 function clearAll() {
   mortgageAmount.value = "";
@@ -19,6 +61,14 @@ function clearAll() {
   totalToPay.textContent = "";
   emptyResultContainer.classList.remove("hidden");
   resultContainer.classList.add("hidden");
+  radioBtnInputs.forEach((input) => {
+    input.checked = false;
+  })
+  radioBtns.forEach((btn) => btn.classList.remove("clicked"));
+  removeRadioError();
+  inputs.forEach((input, index) => {
+    removeError(index)
+  })
 }
 
 function toggleActive(element) {
@@ -26,8 +76,7 @@ function toggleActive(element) {
   element.classList.add("clicked");
 }
 
-function calculateRepayments(e) {
-  e.preventDefault();
+function calculateRepayments() {
   let mortgageAmountValue = parseFloat(mortgageAmount.value);
   let interestRateValue = parseFloat(interestRate.value) / 100 / 12; /* Monthly Interest Rate  */
   let mortgageTermValue = parseInt(mortgageTerm.value);
@@ -36,11 +85,7 @@ function calculateRepayments(e) {
 
   emptyResultContainer.classList.add("hidden");
   resultContainer.classList.remove("hidden");
-
-  if(isNaN(mortgageAmountValue) || isNaN(mortgageTermValue) || isNaN(interestRateValue)) {
-    alert("please fill in all fields with valid numbers");
-    return;
-  }
+  
   if (repayement.checked) {
     let r = interestRateValue 
     monthlyPayment = 
@@ -50,12 +95,35 @@ function calculateRepayments(e) {
     monthlyPayment = mortgageAmountValue * interestRateValue
   }
    monthlyPaymentText.textContent = `£${monthlyPayment.toFixed(2)}`;
-   totalToPay.textContent = `£${(monthlyPayment * mortgageTermMonths).toFixed(2)}`
+   totalToPay.textContent = `£${(monthlyPayment * mortgageTermMonths).toFixed(2)}`;
 }
 
 radioBtns.forEach((btn) => {
   btn.addEventListener("click", () => toggleActive(btn))
 })
 
-myForm.addEventListener("submit", calculateRepayments);
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (checkForError()) {
+    return;
+  }
+  calculateRepayments();
+});
+
 clearBtn.addEventListener("click", clearAll);
+inputs.forEach((input, index) => {
+  input.addEventListener("focus", () => removeError(index))
+})
+radioBtns.forEach(container => {
+  container.addEventListener("click", () => {
+    const radioBtn = container.querySelector("input[type='radio']");
+    if (radioBtn) {
+      radioBtn.checked = true;
+      removeRadioError();
+    }
+  })
+})
+
+
+
+
